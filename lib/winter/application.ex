5c928fe10287de76bc1a5cdd6ed3196bf7a25e30
Miguel Diaz <diazmiiguel@gmail.com>
2021-10-26 09:59:51 -0500

    adds config for a distributed workload

--- lib/winter/application.ex
+++ lib/winter/application.ex
@@ -7,14 +7,18 @@ defmodule Winter.Application do
 
   @impl true
   def start(_type, _args) do
-    children = [
-      Winter.TableManager,
-      {Task.Supervisor, name: Winter.ReceptorSupervisor},
-      {Registry, name: Registry.TableRegistry, keys: :unique},
+    cluster_config = check_distributed_flag()
+
+    set_children = [
+      {Task.Supervisor, name: Winter.ReceptorTaskSupervisor},
+      {Horde.Registry, name: Horde.Registry.TableRegistry, keys: :unique, members: :auto},
       Supervisor.child_spec({Task, fn -> Winter.Receptor.accept(receptor_port()) end},
-        restart: :permanent
-      )
+        restart: :transient
+      ),
+      Winter.TableManager
     ]
+
+    children = cluster_config ++ set_children
 
     opts = [strategy: :one_for_one, name: Winter.Supervisor]
     Supervisor.start_link(children, opts)
@@ -24,5 +28,19 @@ defmodule Winter.Application do
     :winter
     |> Application.get_env(:receptor_port)
     |> String.to_integer()
+  end
+
+  # Add Cluster.Supervisor children and config only if the distributed config is set
+  defp check_distributed_flag do
+    :winter
+    |> Application.get_env(:is_distributed, false)
+    |> build_cluster_config()
+  end
+
+  defp build_cluster_config(false), do: []
+
+  defp build_cluster_config(true) do
+    topologies = Application.get_env(:libcluster, :topologies)
+    [{Cluster.Supervisor, [topologies, [name: Winter.ClusterSupervisor]]}]
   end
 end
--- lib/winter/table_manager.ex
+++ lib/winter/table_manager.ex
@@ -1,20 +1,23 @@
 defmodule Winter.TableManager do
   @moduledoc false
 
-  use DynamicSupervisor
+  use Horde.DynamicSupervisor
 
   @spec start_link(any()) :: Supervisor.on_start()
   def start_link(args) do
-    DynamicSupervisor.start_link(__MODULE__, args, name: __MODULE__)
+    Horde.DynamicSupervisor.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   @impl true
   def init(_) do
-    DynamicSupervisor.init(strategy: :one_for_one)
+    Horde.DynamicSupervisor.init(strategy: :one_for_one, members: :auto)
   end
 
+  @doc """
+  Initiates a table, this table will be distributed among other nodes through this supervisor. Whether or not
+  """
   @spec init_table(String.t()) :: DynamicSupervisor.on_start_child()
   def init_table(table_name) do
-    DynamicSupervisor.start_child(__MODULE__, {Winter.Table, table_name: table_name})
+    Horde.DynamicSupervisor.start_child(__MODULE__, {Winter.Table, table_name: table_name})
   end
 end
